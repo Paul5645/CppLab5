75274f8480dea935902a22e2e40ad0555912d867
Paul <pauljanvelyan1712@gmail.com>
2023-12-17 23:35:12 +0300

    задание 2

--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -1,11 +1,22 @@
 import Math;
 #include <cmath>
 namespace Math {
+	Complex f(const Complex& z) {
+		// Используем формулу a^b = e^(b ln(a)), где а - рациональное число, а b - комплексное
+		double im = std::pow(3 * exp(1.0),-z);
+		return 2 + (im)i;
+	}
+	Rational f(const Rational& r) {
+		Rational a = Rational(1, 2);
+
+
+	}
+	double f(double x) {
+		return(2 + 1.5 * exp(-x));
+	}
+
 	int main()
 	{
-		Complex foon(const Complex & z) {
-			return 2 + (3 * exp(1) ^ (-z))i;
-		}
 		return 0;
 	}
 }	
